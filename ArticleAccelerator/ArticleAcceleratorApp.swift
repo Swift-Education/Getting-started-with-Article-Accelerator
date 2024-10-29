e8f8f09bfd612f49173862b35164343f8453f61d
kangddong <rkdehddud96@naver.com>
2024-10-29 17:36:41 +0900

    Initial Commit

---
+++ ArticleAccelerator/ArticleAcceleratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleAcceleratorApp.swift
+//  ArticleAccelerator
+//
+//  Created by 강동영 on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ArticleAcceleratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
